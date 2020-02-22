2f47fa8b26d00a245561f25e5d047f7cf1acafac
nareshit <nareshit@192.168.100.5>
2020-02-22 12:14:43 +0530

    WH 8596 Update Emp Module

--- src/main/java/in/nit/model/Employee.java
+++ src/main/java/in/nit/model/Employee.java
@@ -1,9 +1,20 @@
 package in.nit.model;
 
-public class Employee {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name="emptab")
+public class Employee {
+	@Id
+	@Column(name="eid")
 	private int empId;
+	@Column(name="ename")
 	private String empName;
+	@Column(name="esal")
 	private double empSal;
 	
 	
